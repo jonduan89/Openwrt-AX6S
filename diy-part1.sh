117b8f6da1a9000c181c6c1fcfb864b3d903e727
jonduan89 <110183405+jonduan89@users.noreply.github.com>
2022-08-05 11:03:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,7 +18,7 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' >>feeds.conf.default
 echo 'src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' >>feeds.conf.default
-echo 'src-git smartdns https://github.com/pymumu/smartdns.git' >>feeds.conf.default
+#echo 'src-git smartdns https://github.com/pymumu/smartdns.git' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small.git' >>feeds.conf.default
 
 sed -i 's#coolsnowwolf/luci#jonduan89/luci#g' feeds.conf.default
@@ -27,6 +27,8 @@ sed -i 's#coolsnowwolf/luci#jonduan89/luci#g' feeds.conf.default
 cd package/lean
 git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge.git
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git
+git clone https://github.com/pymumu/smartdns/tree/master/package/openwrt
+git clone https://github.com/pymumu/smartdns/tree/master/package/luci-compat
 
 # Add passwall
 #git clone https://github.com/jonduan89/luci-app-passwall.git
